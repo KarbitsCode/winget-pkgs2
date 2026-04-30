554775a59baa45a20fed687c8a9307847b60e974
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-05-01 07:29:43 +0800

    Support for listing zip contents

--- Tools/InstallerUrlChecker.ps1
+++ Tools/InstallerUrlChecker.ps1
@@ -7,14 +7,23 @@ $fn = New-TemporaryFile
 
 $rs = Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing -PassThru
 $rs.Headers.Keys | ForEach-Object {
-    "$($_): $($rs.Headers[$_])"
+    Write-Host "$($_): $($rs.Headers[$_])"
 }
-""
+Write-Host ""
 $fl = Get-Item $fn
-$fl.VersionInfo.PSObject.Properties | ForEach-Object {
-	"$($_.Name): $($_.Value)"
+try {
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $zip = [System.IO.Compression.ZipFile]::OpenRead($fl.FullName)
+    $zip.Entries | ForEach-Object {
+        Write-Host "File: $($_.FullName) ($($_.CompressedLength) / $($_.Length) ($([math]::Round(($_.CompressedLength / $_.Length)*100, 2))%))"
+    }
+    $zip.Dispose()
+} catch {
+    $fl.VersionInfo.PSObject.Properties | ForEach-Object {
+        Write-Host "$($_.Name): $($_.Value)"
+    }
 }
-""
-"SHA256: $($(Get-FileHash -Path $fn -Algorithm 'sha256' | ForEach-Object Hash).ToUpper())"
+Write-Host ""
+Write-Host "SHA256: $($(Get-FileHash -Path $fn -Algorithm 'sha256' | ForEach-Object Hash).ToUpper())"
 
 Remove-Item $fn -Force -ErrorAction Ignore
