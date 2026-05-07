5affb3ea7edbd6b0f281ab99a933082d883672f3
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-05-07 18:04:09 +0800

    Support bulk urls

--- Tools/InstallerUrlChecker.ps1
+++ Tools/InstallerUrlChecker.ps1
@@ -1,29 +1,31 @@
 param(
-    [Parameter(Mandatory = $true, Position = 0)]
-    [string]$Url
+    [Parameter(Mandatory = $true, Position = 0, ValueFromRemainingArguments = $true)]
+    [string[]]$Urls
 )
 
-$fn = New-TemporaryFile
-
-$rs = Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing -PassThru
-$rs.Headers.Keys | ForEach-Object {
-    Write-Host "$($_): $($rs.Headers[$_])"
-}
-Write-Host ""
-$fl = Get-Item $fn
-try {
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $zip = [System.IO.Compression.ZipFile]::OpenRead($fl.FullName)
-    $zip.Entries | ForEach-Object {
-        Write-Host "File: $($_.FullName) ($($_.CompressedLength) / $($_.Length) ($([math]::Round(($_.CompressedLength / [math]::Max($_.Length, 1))*100, 2))%))"
+foreach ($Url in $Urls) {
+    Write-Host "Checking $($Url)..." -ForegroundColor Yellow
+    $fn = New-TemporaryFile
+    $rs = Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing -PassThru
+    $rs.Headers.Keys | ForEach-Object {
+        Write-Host "$($_): $($rs.Headers[$_])"
     }
-    $zip.Dispose()
-} catch {
-    $fl.VersionInfo.PSObject.Properties | ForEach-Object {
-        Write-Host "$($_.Name): $($_.Value)"
+    Write-Host ""
+    $fl = Get-Item $fn
+    try {
+        Add-Type -AssemblyName System.IO.Compression.FileSystem
+        $zip = [System.IO.Compression.ZipFile]::OpenRead($fl.FullName)
+        $zip.Entries | ForEach-Object {
+            Write-Host "File: $($_.FullName) ($($_.CompressedLength) / $($_.Length) ($([math]::Round(($_.CompressedLength / [math]::Max($_.Length, 1))*100, 2))%))"
+        }
+        $zip.Dispose()
+    } catch {
+        $fl.VersionInfo.PSObject.Properties | ForEach-Object {
+            Write-Host "$($_.Name): $($_.Value)"
+        }
     }
+    Write-Host ""
+    Write-Host "SHA256: $($(Get-FileHash -Path $fn -Algorithm 'sha256' | ForEach-Object Hash).ToUpper())"
+    Write-Host ""
+    Remove-Item $fn -Force -ErrorAction Ignore
 }
-Write-Host ""
-Write-Host "SHA256: $($(Get-FileHash -Path $fn -Algorithm 'sha256' | ForEach-Object Hash).ToUpper())"
-
-Remove-Item $fn -Force -ErrorAction Ignore
