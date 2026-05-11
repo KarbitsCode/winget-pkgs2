cb063f81e1100f4b12c964c98f3b472ba742c119
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-05-11 18:59:17 +0800

    Add update scripts

--- update/7zip_update.bat
+++ update/7zip_update.bat
@@ -22,3 +22,18 @@ komac update 7zip.7zip ^
     "https://7-zip.org/a/7z%SHORT_VERSION%-arm64.exe|arm64" ^
     "https://7-zip.org/a/7z%SHORT_VERSION%.msi|x86" ^
     "https://7-zip.org/a/7z%SHORT_VERSION%-x64.msi|x64"
+
+if not "%ERRORLEVEL%"=="0" (
+  komac update 7zip.7zip ^
+    --output . ^
+    --skip-pr-check ^
+    --version %VERSION% ^
+    --release-notes-url https://github.com/ip7z/7zip/releases/tag/%VERSION% ^
+    --urls ^
+      "https://sourceforge.net/projects/sevenzip/files/7-Zip/%VERSION%/7z%SHORT_VERSION%.exe|x86" ^
+      "https://sourceforge.net/projects/sevenzip/files/7-Zip/%VERSION%/7z%SHORT_VERSION%-x64.exe|x64" ^
+      "https://sourceforge.net/projects/sevenzip/files/7-Zip/%VERSION%/7z%SHORT_VERSION%-arm.exe|arm" ^
+      "https://sourceforge.net/projects/sevenzip/files/7-Zip/%VERSION%/7z%SHORT_VERSION%-arm64.exe|arm64" ^
+      "https://sourceforge.net/projects/sevenzip/files/7-Zip/%VERSION%/7z%SHORT_VERSION%.msi|x86" ^
+      "https://sourceforge.net/projects/sevenzip/files/7-Zip/%VERSION%/7z%SHORT_VERSION%-x64.msi|x64"
+)
