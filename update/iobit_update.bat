22f89a234cfbb0f7688f8929b09ba9c4c1ac1587
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-06-18 11:27:39 +0800

    Add update scripts

--- update/iobit_update.bat
+++ update/iobit_update.bat
@@ -9,12 +9,14 @@ if "%~1"=="" (
 
 set "PKGNAME=%~1"
 set "VERSION=%~2"
+for /f "tokens=1,2,3 delims=." %%A in ("%VERSION%") do set "SHORT_VERSION=%%A.%%B.%%C"
 
 for /f "usebackq delims=" %%A in (`
   wingetcreate show IObit.%PKGNAME% ^| powershell -NoLogo -NoProfile -Command ^
     "$text = $input | Out-String;" ^
     "if ($text -match 'Architecture:\s*(\S+)') { Write-Output ('ARCH=' + $matches[1]) };" ^
-    "if ($text -match 'InstallerUrl:\s*(\S+)') { Write-Output ('URL=' + $matches[1]) }"
+    "if ($text -match 'InstallerUrl:\s*(\S+)') { Write-Output ('URL=' + $matches[1]) }" ^
+    "if ($text -match 'DisplayVersion:\s*(\S+)') { Write-Output ('DISPLAY_VERSION=' + $matches[1]) }"
 `) do set "%%A"
 
 for /f "usebackq delims=" %%B in (`
@@ -25,7 +27,11 @@ for /f "usebackq delims=" %%B in (`
     "Write-Output ('RELEASE_DATE=' + $reldate)"
 `) do set "%%B"
 
+set "OTHER_ARGS="
+if defined DISPLAY_VERSION set "OTHER_ARGS=--display-version %SHORT_VERSION%"
+
 wingetcreate update IObit.%PKGNAME% ^
   --version %VERSION% ^
   --release-date %RELEASE_DATE% ^
-  --urls "%URL%|%ARCH%"
+  --urls "%URL%|%ARCH%" ^
+  %OTHER_ARGS%
