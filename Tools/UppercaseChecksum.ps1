e36d220a1b083384239452f5074642ccd7f9d573
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2026-09-03 14:13:25 +0800

    Tidy up script

--- Tools/UppercaseChecksum.ps1
+++ Tools/UppercaseChecksum.ps1
@@ -4,27 +4,27 @@ param(
 )
 
 function Get-RemoteChecksum {
-	param(
-		[string]$Url,
-		[string]$Algorithm = 'sha256'
-	)
+    param(
+        [string]$Url,
+        [string]$Algorithm = 'sha256'
+    )
 
-	$fn = New-TemporaryFile
-	try {
-		if (Get-Command curl.exe -ErrorAction SilentlyContinue) {
-			& curl.exe -L $Url -o $fn
-			if ($LASTEXITCODE -ne 0) {
-				throw "curl.exe failed with exit code $LASTEXITCODE"
-			}
-		}
-	} catch {
-		Write-Warning "curl.exe failed: $_"
-		Write-Warning "Falling back to Invoke-WebRequest..."
-		Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing
-	}
-	$res = Get-FileHash $fn -Algorithm $Algorithm | ForEach-Object Hash
-	Remove-Item $fn -Force -ErrorAction Ignore
-	return $res.ToLower()
+    $fn = New-TemporaryFile
+    try {
+        if (Get-Command curl.exe -ErrorAction SilentlyContinue) {
+            & curl.exe -L $Url -o $fn
+            if ($LASTEXITCODE -ne 0) {
+                throw "curl.exe failed with exit code $LASTEXITCODE"
+            }
+        }
+    } catch {
+        Write-Warning "curl.exe failed: $_"
+        Write-Warning "Falling back to Invoke-WebRequest..."
+        Invoke-WebRequest $Url -OutFile $fn -UseBasicParsing
+    }
+    $res = Get-FileHash $fn -Algorithm $Algorithm | ForEach-Object Hash
+    Remove-Item $fn -Force -ErrorAction Ignore
+    return $res.ToLower()
 }
 
 Write-Host $(Get-RemoteChecksum -Url $Target -Algorithm $Type).ToUpper() -ForegroundColor Yellow
